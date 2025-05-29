f939611d5ebed6ce74ea1862fe29ec3e0f782f63
Roberto Alvarez <robertoalvarez3@hotmail.es>
2025-05-29 15:47:22 +0200

    vamos static

--- tienda_juegos/settings.py
+++ tienda_juegos/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -117,9 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+# Ruta de archivos estáticos en desarrollo (por ejemplo /static/images)
 STATICFILES_DIRS = [
-    BASE_DIR / "static",  # Asegúrate de que esté en la raíz del proyecto
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 # Default primary key field type
